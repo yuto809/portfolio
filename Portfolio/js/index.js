900769d3e497af9e6e950a72f24c933e9591b92d
ynaka <family_at_home_6_2@live.jp>
2021-05-07 06:32:18 +0900

    Create Portfolio File

---
+++ Portfolio/js/index.js
@@ -0,0 +1,66 @@
+
+// JQuery
+$(function(){
+
+  $('.about-list-item').click(
+    function()
+    {
+      var $answer = $(this).find('.about-answer');
+
+      if($answer.hasClass('open'))
+      {
+        $answer.removeClass('open');
+        $answer.slideUp();
+      }
+      else
+      {
+        $answer.addClass('open'); 
+        // slideDownメソッドを用いて、$answerを表示してください
+        $answer.slideDown();
+      }
+    }
+  );
+
+  $('.sample').hover(
+    function()
+    {
+      // $('.mask').slideDown();
+      // $(this).find('.sample-text').addClass('text-active'); 
+      $(this).find('.mask').slideDown();
+      $(this).find('.sample-text').fadeIn();
+
+    },
+    function()
+    {
+      // $('.mask').slideUp();
+      // $(this).find('.sample-text').removeClass('text-active'); 
+      $(this).find('.mask').slideUp();
+      $(this).find('.sample-text').fadeOut();
+    }
+  );
+});
+
+const sampleArray = [
+    {src: 'img/top.jpg', msg: 'sample1', size:'50%'},
+    {src: 'img/top.jpg', msg: 'sample2', size:'50%'},
+    {src: 'img/top.jpg', msg: 'sample3', size:'50%'},
+    {src: 'img/top.jpg', msg: 'sample4', size:'50%'},
+];
+
+
+
+
+// for (let i = 0; i < sampleArray.length(); i++)
+// {
+//     let thumbImage = document.createElement('img');
+//     thumbImage.setAttribute('src', album[i].src);
+//     thumbImage.setAttribute('alt', album[i].msg);
+//     thumbImage.setAttribute('width', album[i].size);
+//     //thumbFlame.insertBefore(thumbImage,null);
+
+
+
+
+
+
+// }
